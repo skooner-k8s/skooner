8b254e81866d84252cb249544809012ae103106d
Eric Herbrandson <eherbrandson@liveearth.com>
2019-04-06 14:35:27 -0500

    Adding sorting by cpu/ram support to nodes view. Some better error handling and messaging. Improved logging on the server. Better proxy handling on server (changeOrigin)

---
+++ client/src/components/nodesPanel.js
@@ -0,0 +1,117 @@
+import _ from 'lodash';
+import React from 'react';
+import Base from './base';
+import {MetadataHeaders, MetadataColumns, TableBody, objectMap} from './listViewHelpers';
+import Sorter from './sorter';
+import {parseCpu, parseRam} from '../utils/unitHelpers';
+
+export default class NodesPanel extends Base {
+    constructor(props) {
+        super(props);
+        this.sortByCpu = this.sortByCpu.bind(this);
+        this.sortByRam = this.sortByRam.bind(this);
+    }
+
+    sortByCpu(item) {
+        const used = getCpuUsed(item, this.props.metrics);
+        if (used == null) return null;
+
+        const available = getCpuAvailable(item);
+        return used / available;
+    }
+
+    sortByRam(item) {
+        const used = getRamUsed(item, this.props.metrics);
+        if (used == null) return null;
+
+        const available = getRamAvailable(item);
+        return used / available;
+    }
+
+    render() {
+        const {items, metrics, sort, filter} = this.props;
+
+        return (
+            <div className='contentPanel'>
+                <table>
+                    <thead>
+                        <tr>
+                            <MetadataHeaders sort={sort} />
+                            <th>Labels</th>
+                            <th><Sorter field={getReadyStatus} sort={sort}>Ready</Sorter></th>
+                            <th><Sorter field={this.sortByCpu} sort={sort}>Cpu</Sorter></th>
+                            <th><Sorter field={this.sortByRam} sort={sort}>Ram</Sorter></th>
+                        </tr>
+                    </thead>
+
+                    <TableBody items={items} filter={filter} sort={sort} colSpan='8' row={x => (
+                        <tr key={x.metadata.uid}>
+                            <MetadataColumns item={x} href={`#/node/${x.metadata.name}`} />
+                            <td>{objectMap(x.metadata.labels)}</td>
+                            <td>{getReadyStatus(x)}</td>
+                            <td><CpuPercent item={x} metrics={metrics} /></td>
+                            <td><RamPercent item={x} metrics={metrics} /></td>
+                        </tr>
+                    )} />
+                </table>
+            </div>
+        );
+    }
+}
+
+function getReadyStatus({status}) {
+    if (!status.conditions) return null;
+
+    const ready = status.conditions.find(y => y.type === 'Ready');
+    return ready && ready.status;
+}
+
+function CpuPercent({item, metrics}) {
+    const used = getCpuUsed(item, metrics);
+    if (used == null) return null;
+
+    const available = getCpuAvailable(item);
+    return <Percent used={used} available={available} />;
+}
+
+function getCpuUsed(item, metrics) {
+    const usage = getUsage(item, metrics);
+    if (!usage) return null;
+
+    return parseCpu(usage.cpu) / 1000000;
+}
+
+function getCpuAvailable(item) {
+    return parseCpu(item.status.capacity.cpu) / 1000000;
+}
+
+function RamPercent({item, metrics}) {
+    const used = getRamUsed(item, metrics);
+    if (used == null) return null;
+
+    const available = getRamAvailable(item);
+    return <Percent used={used} available={available} />;
+}
+
+function getRamUsed(item, metrics) {
+    const usage = getUsage(item, metrics);
+    if (!usage) return null;
+
+    return parseRam(usage.memory);
+}
+
+function getRamAvailable(item) {
+    return parseRam(item.status.capacity.memory);
+}
+
+function Percent({used, available}) {
+    const percent = _.round(used / available * 100, 1);
+    const className = percent >= 85 ? 'contentPanel_warn' : undefined;
+    return (<span className={className}>{`${percent}%`}</span>);
+}
+
+function getUsage(item, metrics) {
+    if (!item || !metrics) return null;
+    const result = metrics[item.metadata.name] || {};
+    return result && result.usage;
+}
--- client/src/views/nodes.js
+++ client/src/views/nodes.js
@@ -2,14 +2,14 @@ import _ from 'lodash';
 import React from 'react';
 import Base from '../components/base';
 import Filter from '../components/filter';
-import Chart from '../components/chart';
+import {defaultSortInfo} from '../components/sorter';
 import NodeStatusChart from '../components/nodeStatusChart';
-import LoadingChart from '../components/loadingChart';
-import {MetadataHeaders, MetadataColumns, TableBody, objectMap} from '../components/listViewHelpers';
-import Sorter, {defaultSortInfo} from '../components/sorter';
 import api from '../services/api';
-import {parseCpu, parseRam, TO_GB, TO_ONE_CPU} from '../utils/unitHelpers';
 import test from '../utils/filterHelper';
+import NodesPanel from '../components/nodesPanel';
+import Chart from '../components/chart';
+import LoadingChart from '../components/loadingChart';
+import {parseCpu, parseRam, TO_GB, TO_ONE_CPU} from '../utils/unitHelpers';
 
 export default class Nodes extends Base {
     state = {
@@ -49,58 +49,14 @@ export default class Nodes extends Base {
                     <RamTotalsChart items={filtered} metrics={filteredMetrics} />
                 </div>
 
-                <div className='contentPanel'>
-                    <table>
-                        <thead>
-                            <tr>
-                                <MetadataHeaders sort={sort} />
-                                <th>Labels</th>
-                                <th><Sorter field={getReadyStatus} sort={sort}>Ready</Sorter></th>
-                                <th>Cpu</th>
-                                <th>Ram</th>
-                                {/* <th><Sorter field={sortByCpu} sort={sort}>Cpu</Sorter></th>
-                                <th><Sorter field={sortByRam} sort={sort}>Ram</Sorter></th> */}
-                            </tr>
-                        </thead>
-
-                        <TableBody items={filtered} filter={filter} sort={sort} colSpan='8' row={x => (
-                            <tr key={x.metadata.uid}>
-                                <MetadataColumns item={x} href={`#/node/${x.metadata.name}`} />
-                                <td>{objectMap(x.metadata.labels)}</td>
-                                <td>{getReadyStatus(x)}</td>
-                                <td><CpuChart item={x} metrics={filteredMetrics} /></td>
-                                <td><RamChart item={x} metrics={filteredMetrics} /></td>
-                            </tr>
-                        )} />
-                    </table>
-                </div>
+                <NodesPanel
+                    sort={sort}
+                    items={filtered}
+                    metrics={filteredMetrics}
+                />
             </div>
         );
     }
-}
-
-// TODO: get sortying by cpu/ram working
-// function sortByCpu(item) {
-//     const used = getCpuUsed(item, filteredMetrics);
-//     if (used == null) return null;
-
-//     const available = getCpuAvailable(item);
-//     return used / available;
-// }
-
-// function sortByRam(item) {
-//     const used = getRamUsed(item, filteredMetrics);
-//     if (used == null) return null;
-
-//     const available = getRamAvailable(item);
-//     return used / available;
-// }
-
-function getReadyStatus({status}) {
-    if (!status.conditions) return null;
-
-    const ready = status.conditions.find(y => y.type === 'Ready');
-    return ready && ready.status;
 }
 
 function CpuTotalsChart({items, metrics}) {
@@ -129,55 +85,6 @@ function RamTotalsChart({items, metrics}) {
             <div className='charts_itemLabel'>GB Ram Used</div>
         </div>
     );
-}
-
-function CpuChart({item, metrics}) {
-    const used = getCpuUsed(item, metrics);
-    if (used == null) return null;
-
-    const available = getCpuAvailable(item);
-    return <Percent used={used} available={available} />;
-}
-
-function getCpuUsed(item, metrics) {
-    const usage = getUsage(item, metrics);
-    if (!usage) return null;
-
-    return parseCpu(usage.cpu) / 1000000;
-}
-
-function getCpuAvailable(item) {
-    return parseCpu(item.status.capacity.cpu) / 1000000;
-}
-
-function RamChart({item, metrics}) {
-    const used = getRamUsed(item, metrics);
-    if (used == null) return null;
-
-    const available = getRamAvailable(item);
-    return <Percent used={used} available={available} />;
-}
-
-function getRamUsed(item, metrics) {
-    const usage = getUsage(item, metrics);
-    if (!usage) return null;
-
-    return parseRam(usage.memory);
-}
-
-function getRamAvailable(item) {
-    return parseRam(item.status.capacity.memory);
-}
-
-function Percent({used, available}) {
-    const percent = _.round(used / available * 100, 1);
-    return (<span>{`${percent}%`}</span>);
-}
-
-function getUsage(item, metrics) {
-    if (!item || !metrics) return null;
-    const result = metrics[item.metadata.name] || {};
-    return result && result.usage;
 }
 
 function getNodeMetrics(nodes, metrics) {
