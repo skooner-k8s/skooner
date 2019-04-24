8e6d22c2a0c9b1e2caea6dcdac72a038ee48b71b
Eric Herbrandson <eherbrandson@liveearth.com>
2019-04-23 21:58:19 -0500

    Creating custom donut chart with animations

--- client/src/components/loadingChart.js
+++ client/src/components/loadingChart.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import ChartistGraph from 'react-chartist';
+import Donut from './donut';
 import LoadingEllipsis from './loadingEllipsis';
-
-const options = {
-    donut: true,
-    donutWidth: 25,
-    donutSolid: true,
-    startAngle: 0,
-    showLabel: false,
-};
 
 export default function Chart() {
     return (
         <div className='charts_donut'>
-            <ChartistGraph data={{series: [0, 0, 1]}} options={options} type='Pie' />
+            <Donut />
             <LoadingEllipsis />
         </div>
     );
