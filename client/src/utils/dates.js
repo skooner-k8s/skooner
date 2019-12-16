7d774198acd7dff08ff56103a6a229d852eab01c
Olivier <volerig@gmail.com>
2019-12-16 19:34:14 +0100

    Use humanize-duration to better format duration (mo, m, ms)

--- client/src/utils/dates.js
+++ client/src/utils/dates.js
@@ -1,5 +1,31 @@
-import timeAgo from 'time-ago';
+import HumanizeDuration from 'humanize-duration'
 
-export default function fromNow(value) {
-    return timeAgo.ago(value, true);
+/**
+ * A simple humanizer to merely differentiate between months and minutes.
+ * (Could be improved later for i18n)
+ */
+const shortEnglishHumanizer = HumanizeDuration.humanizer({
+    language: 'shortEn',
+    languages: {
+      shortEn: {
+        y: () => 'y',
+        mo: () => 'mo',
+        w: () => 'w',
+        d: () => 'd',
+        h: () => 'h',
+        m: () => 'm',
+        s: () => 's',
+        ms: () => 'ms',
+      }
+    }
+  })
+
+ /**
+  * @param epochtimestampMs an epoch timestamp value (in ms)
+  * @returns a "humanized" duration from now
+  * 
+  */ 
+export default function fromNow(epochtimestampMs) {
+    var diff = Date.now() - new Date(epochtimestampMs).getTime()
+    return shortEnglishHumanizer(diff).split(",")[0]
 }
