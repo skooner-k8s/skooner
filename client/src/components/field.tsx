2f8c9af46bf7bb4466f46792390b8009e148ed1c
Eric Blackburn <eblackburn@indeed.com>
2020-08-05 15:56:53 -0500

    Converted src/components/field.js to tsx

--- client/src/components/field.tsx
+++ client/src/components/field.tsx
@@ -1,7 +1,13 @@
 import './field.scss';
 import React from 'react';
 
-const Field = ({name, value, children}) => (
+type FieldProps = {
+    name: string
+    value: string | null
+    children: React.ReactNode | null
+}
+
+const Field = ({name, value, children}: FieldProps) => (
     <div className='field_set'>
         <label className='field_name'>{name}</label>
         <span className='field_value'>{value || children}</span>
