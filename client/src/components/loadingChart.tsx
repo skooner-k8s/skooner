5cf80ec5953141e2eeaaec1323e6b054587dbd77
Eric Blackburn <eblackburn@indeed.com>
2020-08-06 16:46:59 -0500

    Fixed loadingChart.tsx compatibility with donut.tsx

--- client/src/components/loadingChart.tsx
+++ client/src/components/loadingChart.tsx
@@ -5,7 +5,7 @@ import LoadingEllipsis from './loadingEllipsis';
 export default function Chart() {
     return (
         <div className='charts_donut'>
-            <Donut />
+            <Donut percent={0} percent2={0} />
             <LoadingEllipsis />
         </div>
     );
