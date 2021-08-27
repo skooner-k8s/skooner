90122d4f729e77587906a27a913d21d2b677c7aa
Yuqiu Wang <yuqiuw@indeed.com>
2021-08-27 11:45:54 -0500

    fix #243
    Signed-off-by: Yuqiu Wang <yuqiuw@indeed.com>

--- client/src/views/prometheusgraph.tsx
+++ client/src/views/prometheusgraph.tsx
@@ -3,7 +3,7 @@ import {XYPlot, XAxis, YAxis, LineSeries, Crosshair, AreaSeries} from 'react-vis
 import Base from '../components/base';
 
 
-export const BASE_HTTP_URL = 'http://localhost:4654/prom';
+export const BASE_HTTP_URL = 'http://localhost:50320';
 
 
 type Props = {
