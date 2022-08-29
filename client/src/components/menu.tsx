f5e0aed16f072cf5dc3b3c9029822f014f94b36d
HenrikDK <commander.john.crichton@gmail.com>
2022-08-29 17:36:57 +0200

    no need for an api call when we just have a static menu

--- client/src/components/menu.tsx
+++ client/src/components/menu.tsx
@@ -2,12 +2,7 @@ import './menu.scss';
 import React from 'react';
 import Base from './base';
 import {getRootPath} from '../router';
-import EditorModal from '../views/editorModal';
-import api from '../services/api';
-import {addHandler} from '../services/auth';
 import ResourceSvg from '../art/resourceSvg';
-import AddSvg from '../art/addSvg';
-import {TODO} from '../utils/types';
 
 interface MenuProps {
     onClick: () => void;
@@ -15,7 +10,6 @@ interface MenuProps {
 }
 
 interface MenuStates {
-    rules: TODO[];
     showAdd: boolean;
 }
 
@@ -29,21 +23,10 @@ interface MenuItemProps {
 
 export default class Menu extends Base<MenuProps, MenuStates> {
     componentDidMount() {
-        this.getRules();
-
-        this.registerApi({
-            status: addHandler(() => this.getRules()),
-        });
-    }
-
-    async getRules() {
-        const {status} = await api.getRules('default');
-        this.setState({rules: status.resourceRules});
     }
 
     render() {
         const {onClick, toggled} = this.props;
-        const {showAdd, rules} = this.state || {};
 
         return (
             <>
@@ -65,21 +48,6 @@ export default class Menu extends Base<MenuProps, MenuStates> {
     }
 }
 
-function canView(resourcesRules: TODO[], ...args: TODO) {
-    if (!resourcesRules) return false;
-
-    return args.some((x: TODO) => canViewResource(resourcesRules, x.resource));
-}
-
-function canViewResource(resourcesRules: TODO[], {group, resource}: {group: TODO, resource: string}) {
-    return resourcesRules.some((x) => {
-        const hasVerb = (x.verbs.includes('list') || x.verbs.includes('*'));
-        const hasGroup = (x.apiGroups.includes(group) || x.apiGroups.includes('*'));
-        const hasResource = (x.resources.includes(resource)) || x.resources.includes('*');
-        return hasVerb && hasGroup && hasResource;
-    });
-}
-
 function MenuItem(props: MenuItemProps) {
     const currentPath = getRootPath();
     const paths = getPaths(props);
@@ -91,30 +59,6 @@ function MenuItem(props: MenuItemProps) {
             <ResourceSvg resource={resource} />
             <span className='menu_title'>{title}</span>
         </a>
-    );
-}
-
-function Group({children = []}: TODO) {
-    if (!Array.isArray(children)) children = [children]; // eslint-disable-line no-param-reassign
-    children = children.filter(Boolean); // eslint-disable-line no-param-reassign
-
-    if (children.length === 0) return null;
-
-    const paths = children.flatMap((x: TODO) => getPaths(x.props));
-
-    const currentPath = getRootPath();
-    const isSelected = paths.some((x: TODO) => x === currentPath);
-
-    return (
-        <div className='menu_group'>
-            <div>
-                {children[0]}
-            </div>
-
-            <div className={isSelected ? 'menu_subMenu menu_subMenuSelected' : 'menu_subMenu'}>
-                {children.slice(1)}
-            </div>
-        </div>
     );
 }
 
