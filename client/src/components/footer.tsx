da49b10ade6f57c4ba45f42ca17583db467f6724
atu <atu@indeed.com>
2022-12-22 15:49:44 -0800

    fix singlequote

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function Footer() {
     if (process.env.REACT_APP_FOOTER) {
-        document.body.style.setProperty("--footer-height", "17px");
+        document.body.style.setProperty('--footer-height', '17px');
         return <div id='footer'>{process.env.REACT_APP_FOOTER}</div>;
     }
     return null;
